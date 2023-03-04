328d1f5e738a4c05ef9cb5d6efa791e447541feb
[mahapatrasonali] <[sonangr2000@gmail.com]>
2023-03-04 23:31:32 +0530

    Create Table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,5 +1,25 @@
 ﻿Create database Payroll_Service
 
+create table employee_payroll
+(
+EmployeeId int primary key identity,
+EmployeeName varchar(255),
+PhoneNumber varchar(255),
+Address varchar(255),
+Department varchar(255),
+Gender varchar(255),
+BasicPay float,
+Deductions float,
+TaxablePay float,
+Tax float,
+NetPay float,
+StartDate Date,
+City varchar(255),
+Country varchar(255),
+)
+
+select * from employee_payroll
+
 SELECT name, database_id, create_date  
 FROM sys.databases;  
 GO
